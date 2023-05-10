926815ca91c2256f22e310bd5e5e58d6ec60b8a0
p.talwar <Pushkar>
2023-05-10 11:49:13 -0600

    added stuff

--- handEnemy.py
+++ handEnemy.py
@@ -1,6 +1,69 @@
-"""
-
-"""
 
 
-class Hand_enemy(ImageSprite):
+import pygame
+from mySprite import mySprite
+
+class HandEnemy(mySprite):
+    """
+    Load and manipulate images
+    """
+    def __init__(self, IMAGE_FILE):
+        mySprite.__init__(self)
+        self.__FILE_LOC = IMAGE_FILE
+        self._SURFACE = pygame.image.load(self.__FILE_LOC).convert_alpha()
+        self.__X_FLIP = False
+
+    def moveWASD(self, KEYS_PRESSED):
+
+        mySprite.moveWASD(self, KEYS_PRESSED)
+        if KEYS_PRESSED[pygame.K_a]:
+            if self.__X_FLIP:
+                self.setFlipX()
+                self.__X_FLIP = False
+        if KEYS_PRESSED[pygame.K_d]:
+            if not self.__X_FLIP:
+                self.setFlipX()
+                self.__X_FLIP = True
+
+    def setScale(self, SCALE_X, SCALE_Y=0):
+        """
+        Resize the image based on a factor
+        :param SCALE_X: float
+        :param SCALE_Y: float
+        :return: None
+        """
+
+        if SCALE_Y == 0:
+            SCALE_Y = SCALE_X
+        self._SURFACE = pygame.transform.scale(
+            self._SURFACE,
+            (self.getWidth()*SCALE_X, self.getHeight()*SCALE_Y)
+        )
+
+    def setFlipX(self):
+        """
+        Flip image on the Y axis
+        :return:
+        """
+        self._SURFACE = pygame.transform.flip(self._SURFACE, True, False)
+
+if __name__ == "__main__":
+    from window import Window
+    pygame.init()
+
+    WINDOW = Window("Image Sprite Test")
+    BUNNY = ImageSprite("images/bunny.png")
+    BUNNY.setSPD(15)
+    BUNNY.setScale(0.5)
+    BUNNY.setFlipX()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        PRESSED_KEYS = pygame.key.get_pressed()
+        BUNNY.moveWASD(PRESSED_KEYS)
+
+        WINDOW.ClearScreen()
+        WINDOW.getSurface().blit(BUNNY.getSurface(), BUNNY.getPOS())
+        WINDOW.updateFrames()
