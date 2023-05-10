edaa1f2f308abb83ae069d28498d4abfcd723609
Mengqi Wang <m.wang55@share.epsb.ca>
2023-05-10 11:47:43 -0600

    player class

--- player.py
+++ player.py
@@ -1,4 +1,21 @@
 """
 title: the player class
-
+author: Mengqi Wang
+date-created: 05/10/2023
 """
+
+from mySprite import mySprite
+
+class Player(mySprite):
+
+    def __init__(self):
+        mySprite.__init__(self)
+
+
+
+
+
+
+
+
+
