68ba90ce80c3c79233423ab6f705163f2e94a113
Mengqi Wang <m.wang55@share.epsb.ca>
2023-05-12 11:38:17 -0600

    shrub

--- player.py
+++ player.py
@@ -10,8 +10,12 @@ class Player(mySprite):
 
     def __init__(self):
         mySprite.__init__(self)
+        self.__FILE_LOC = IMAGE_FILE
+        self._SURFACE = pygame.image.load(self.__FILE_LOC).convert_alpha()
+        self.__X_FLIP = False
 
 
+if __name__ == "__main__":
 
 
 
