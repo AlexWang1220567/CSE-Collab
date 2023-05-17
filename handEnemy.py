0cc0cb982247c37a0c305e46be71e2f0de1ecda6
p.talwar <Pushkar>
2023-05-17 11:40:45 -0600

    Finished Hand enemy

--- handEnemy.py
+++ handEnemy.py
@@ -10,7 +10,7 @@ import random
 import pygame
 from mySprite import mySprite
 from window import Window
-from  player import Player
+from player import Player
 
 
 class HandEnemy(mySprite):
@@ -64,7 +64,7 @@ class HandEnemy(mySprite):
             self._DIR_Y = -1
         if self._Y < MIN_HIEGTH:
             self._DIR_Y = 1
-        self.__POS = (self._X, self._Y)
+        self.setPosition((self._X, self._Y))
 
     def fall(self):
         self._Y += self._SPD +5
@@ -78,7 +78,7 @@ class HandEnemy(mySprite):
             )
         )
 
-    def setFireHand(self):
+    def setFireHand(self, WINDOW):
         self.setPosition(
             (
                 WINDOW.getWidth()//2 - self.getWidth()//2,
@@ -89,16 +89,24 @@ class HandEnemy(mySprite):
 
 
 class Laser(mySprite):
-    def __init__(self, IMAGE_FILE):
+
+    def __init__(self, IMAGE_FILE, SPD=0):
         mySprite.__init__(self)
         self.__FILE_LOC = IMAGE_FILE
         self._SURFACE = pygame.image.load(self.__FILE_LOC).convert_alpha()
-        self.__X_FLIP = True
+        self.__X_FLIP = False
+        self._SPD_Y = SPD
 
     def set_X_Flip(self, BOOL=True):
         self.__X_FLIP = BOOL
 
-    def resetLaserPOS(self):
+    def setDIR_X(self, INT):
+        self._DIR_X = INT
+
+    def setDIR_Y(self, INT):
+        self._DIR_Y = INT
+
+    def resetLaserPOS(self, FIRE_HAND):
         self.setPosition(
             (
                 FIRE_HAND.getPOS()[0] + FIRE_HAND.getWidth()//2,
@@ -109,44 +117,149 @@ class Laser(mySprite):
 
     def laserMovment(self):
         self._X += self._SPD * self._DIR_X
+        self._Y += self._SPD_Y * self._DIR_Y
         self.setPosition((self._X, self._Y))
+
+    def setScale(self, SCALE_X, SCALE_Y=0):
+        """
+        Resize the image based on a factor
+        :param SCALE_X: float
+        :param SCALE_Y: float
+        :return: None
+        """
+
+        if SCALE_Y == 0:
+            SCALE_Y = SCALE_X
+        self._SURFACE = pygame.transform.scale(
+            self._SURFACE,
+            (self.getWidth()*SCALE_X, self.getHeight()*SCALE_Y)
+        )
+
+    def setFlipX(self):
+        """
+        Flip image on the Y axis
+        :return:
+        """
+        self._SURFACE = pygame.transform.flip(self._SURFACE, True, False)
+
+    def setSPD_Y(self, SPD):
+        self._SPD_Y = SPD
+
+    def shootLaser(self, HAND):
+        self._X += self._SPD * self._DIR_X
+        self.setPosition((self._X, self._Y))
+
+class Hand_Engine:
+    def __init__(self):
+        pass
+
+    def run(self):
+        ### Time since clocks
+        time_since_fall = 0
+        time_since_laser = 0
+        clock = pygame.time.Clock()
+
+        ### Objects
+        WINDOW = Window("Hand enemy Test")
+        BUNNY = HandEnemy("sprite_images/thumb.png")
+        FIRE_HAND = HandEnemy("sprite_images/fireHand.png")
+        LASER_HAND = HandEnemy("sprite_images/shrubs.png")
+        LASER = Laser("sprite_images/fire.png")
+        LASER2 = Laser("sprite_images/fire.png")
+        LASER_BEAM = Laser("sprite_images/laser (1).png")
+
+        ### Set scales
+        LASER_HAND.setScale(0.125)
+        LASER.setScale(0.5)
+        LASER2.setScale(0.5)
+        BUNNY.setScale(1)
+
+        ### Set SPD
+        LASER_HAND.setSPD(10)
+        LASER.setSPD(15)
+        LASER.setSPD_Y(8)
+        LASER2.setSPD(15)
+        LASER2.setSPD_Y(8)
+        BUNNY.setSPD(15)
+        LASER_BEAM.setSPD(30)
+
+        ### Set DIR
+        LASER.setDIR_Y((-1))
+        LASER2.setDIR_Y((-1))
+        LASER2.setDIR_X((-1))
+        LASER_BEAM.setDIR_X(-1)
+
+        ### Set Flips
+        BUNNY.setFlipX()
+        LASER2.setFlipX()
+        LASER_BEAM.setFlipX()
+
+        ### Set POS
+        FIRE_HAND.setFireHand(WINDOW)
+        LASER.resetLaserPOS(FIRE_HAND)
+        LASER2.resetLaserPOS(FIRE_HAND)
+        BUNNY.setPosition(
+            (
+                0, 0
+            )
+        )
+
+        LASER_HAND.setPosition(
+            (
+                WINDOW.getWidth() - LASER_HAND.getWidth(),
+                WINDOW.getHeight()//2
+            )
+        )
+
+        LASER_BEAM.setPosition(
+            (
+                LASER_HAND.getPOS()[0],
+                LASER_HAND.getPOS()[1] + LASER_HAND.getHeight()//2
+            )
+        )
+
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+            TIME = clock.tick()
+            time_since_fall += TIME
+            time_since_laser += TIME
+            if time_since_fall > 250:
+                BUNNY.fall()
+                if BUNNY.getPOS()[1] >= WINDOW.getHeight():
+                    time_since_fall = 0
+                    BUNNY.moveThumb(WINDOW)
+            LASER.laserMovment()
+            if LASER.getPOS()[0] > WINDOW.getWidth():
+                LASER.resetLaserPOS(FIRE_HAND)
+            LASER2.laserMovment()
+            if LASER2.getPOS()[0] < 0:
+                LASER2.resetLaserPOS(FIRE_HAND)
+
+            if time_since_laser > 30:
+                LASER_BEAM.shootLaser(LASER_HAND)
+                if LASER_BEAM.getPOS()[0] < (0 - LASER_BEAM.getWidth()):
+                    time_since_laser = 0
+                    LASER_BEAM.setPosition(
+                        (
+                            LASER_HAND.getPOS()[0],
+                            LASER_HAND.getPOS()[1] + LASER_HAND.getHeight() // 2
+                        )
+                    )
+            LASER_HAND.bounceY(LASER_HAND.getHeight(), WINDOW.getHeight())
+            WINDOW.ClearScreen()
+            WINDOW.getSurface().blit(BUNNY.getSurface(), BUNNY.getPOS())
+            WINDOW.getSurface().blit(FIRE_HAND.getSurface(), FIRE_HAND.getPOS())
+            WINDOW.getSurface().blit(LASER.getSurface(), LASER.getPOS())
+            WINDOW.getSurface().blit(LASER2.getSurface(), LASER2.getPOS())
+            WINDOW.getSurface().blit(LASER_BEAM.getSurface(), LASER_BEAM.getPOS())
+            WINDOW.getSurface().blit(LASER_HAND.getSurface(), LASER_HAND.getPOS())
+            WINDOW.updateFrames()
 
 if __name__ == "__main__":
     pygame.init()
-    time_since_action = 0
-    clock = pygame.time.Clock()
-    WINDOW = Window("Hand enemy Test")
-    BUNNY = HandEnemy("sprite_images/thumb.png")
-    FIRE_HAND = HandEnemy("sprite_images/fireHand.png")
-    LASER = Laser("sprite_images/laser (1).png")
-    LASER.setSPD(15)
-    BUNNY.setSPD(15)
-    BUNNY.setScale(1)
-    BUNNY.setFlipX()
-    FIRE_HAND.setFireHand()
-    LASER.resetLaserPOS()
-    BUNNY.setPosition(
-        (
-            0,0
-        )
-    )
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-        TIME = clock.tick()
-        time_since_action += 1
-        if time_since_action > 250:
-            BUNNY.fall()
-            if BUNNY.getPOS()[1] >= WINDOW.getHeight():
-                time_since_action = 0
-                BUNNY.moveThumb(WINDOW)
-        LASER.laserMovment()
-        if LASER.getPOS()[0] > WINDOW.getWidth():
-            LASER.resetLaserPOS()
-        WINDOW.ClearScreen()
-        WINDOW.getSurface().blit(BUNNY.getSurface(), BUNNY.getPOS())
-        WINDOW.getSurface().blit(FIRE_HAND.getSurface(), FIRE_HAND.getPOS())
-        WINDOW.getSurface().blit(LASER.getSurface(), LASER.getPOS())
-        WINDOW.updateFrames()
+    GAME = Hand_Engine()
+    GAME.run()
